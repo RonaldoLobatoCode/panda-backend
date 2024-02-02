fd216d0aabc64cb47c195f131a62a25ad9808330
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-02-02 14:03:10 -0500

    Finalizado el desarrollo de la interfaz CamionRepository en el paquete 'repositories/conductor'

--- src/main/java/org/example/panda/conductor/repositories/CamionRepository.java
+++ src/main/java/org/example/panda/conductor/repositories/CamionRepository.java
@@ -1,4 +1,9 @@
 package org.example.panda.conductor.repositories;
 
-public interface CamionRepository {
+import org.example.panda.conductor.entities.Camion;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CamionRepository extends JpaRepository<Camion, Integer> {
 }
