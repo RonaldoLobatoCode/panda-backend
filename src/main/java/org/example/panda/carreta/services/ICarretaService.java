4eeea04c2bea71a6c32479b22e480a3de369cfb8
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-02-02 18:30:05 -0500

    Añadiendo interfaz ICarretaService para operaciones CRUD de Carreta

--- src/main/java/org/example/panda/carreta/services/ICarretaService.java
+++ src/main/java/org/example/panda/carreta/services/ICarretaService.java
@@ -1,4 +1,13 @@
 package org.example.panda.carreta.services;
 
-public class ICarretaService {
+import org.example.panda.carreta.dtos.CarretaDto;
+import org.example.panda.carreta.dtos.CarretaResponse;
+
+public interface ICarretaService {
+
+    CarretaDto createCarreta(CarretaDto carretaDto);
+    CarretaResponse listCarreta(int numeroDePagina, int medidaDePagina, String ordenarPor, String sortDir);
+    CarretaDto listCarretaById(Integer id);
+    CarretaDto updateCarreta(Integer id, CarretaDto carretaDto);
+    void deleteCarreta(Integer id);
 }
