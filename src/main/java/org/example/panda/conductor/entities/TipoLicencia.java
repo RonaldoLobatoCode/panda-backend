8dcf3ea16ac5972cd1c2992b73393b1505fae151
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-02-02 11:40:24 -0500

    Finalizado el desarrollo de la clase TipoLicencia en el paquete 'entity/conductor'

--- src/main/java/org/example/panda/conductor/entities/TipoLicencia.java
+++ src/main/java/org/example/panda/conductor/entities/TipoLicencia.java
@@ -1,4 +1,46 @@
 package org.example.panda.conductor.entities;
 
-public class TipoLicencia {
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "tipos_licencia")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TipoLicencia implements Serializable{
+
+    public enum  TipoLicenciaEnum{
+        A3,
+        A4,
+        A5,
+        B1,
+        B2,
+        B3,
+        B4,
+        C1,
+        C2,
+        C3,
+        C4,
+        D1,
+        D2,
+        D3,
+        D4
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private LOng id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "tipo_licencia", nullable = false)
+    private TipoLicenciaEnum tipoLicencia;
 }
