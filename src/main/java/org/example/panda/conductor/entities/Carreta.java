c3caddde97d06b8148669c9e459c8b144d926971
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-02-02 11:56:04 -0500

    Refactorizando el desarrollo de la clase Carreta en el paquete 'entity/conductor

--- src/main/java/org/example/panda/conductor/entities/Carreta.java
+++ src/main/java/org/example/panda/conductor/entities/Carreta.java
@@ -1,4 +1,33 @@
 package org.example.panda.conductor.entities;
 
-public class Carreta {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "carretas")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Carreta implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "marca", nullable = false, length = 50)
+    private String marca;
+
+    @Column(name = "capacidad_carga", nullable = false)
+    private BigDecimal capacidadCarga;
+
+    @Column(name = "año_fabricacion", nullable = false)
+    private int añoFabricacion;
 }
