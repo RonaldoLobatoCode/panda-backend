8efbeb02ec8651fb603cfbb0516684206a7768d9
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-02-02 13:56:09 -0500

    Finalizado el desarrollo de la interfaz CitasViajeRepository en el paquete 'repositories/conductor'

--- src/main/java/org/example/panda/conductor/repositories/CitasViajeRepository.java
+++ src/main/java/org/example/panda/conductor/repositories/CitasViajeRepository.java
@@ -1,4 +1,9 @@
 package org.example.panda.conductor.repositories;
 
-public interface CitasViajeRepository {
+import org.example.panda.conductor.entities.CitasViaje;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CitasViajeRepository extends JpaRepository<CitasViaje, Integer> {
 }
