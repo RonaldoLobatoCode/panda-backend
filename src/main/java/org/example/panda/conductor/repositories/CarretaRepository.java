82bfafc8748e4cae0440a14fb2ca676b21cdd205
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-02-02 13:59:37 -0500

    Finalizado el desarrollo de la interfaz CarretaRepository en el paquete 'repositories/conductor'

--- src/main/java/org/example/panda/conductor/repositories/CarretaRepository.java
+++ src/main/java/org/example/panda/conductor/repositories/CarretaRepository.java
@@ -1,4 +1,9 @@
 package org.example.panda.conductor.repositories;
 
-public interface CarretaRepository {
+import org.example.panda.conductor.entities.Carreta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CarretaRepository extends JpaRepository<Carreta, Integer> {
 }
