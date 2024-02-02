310c881ddf4c9ee04c555b3d539620e9c1a2bc45
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-02-02 12:42:57 -0500

    Finalizado el desarrollo de la clase Conductor en el paquete 'entity/conductor'

--- src/main/java/org/example/panda/conductor/entities/Camion.java
+++ src/main/java/org/example/panda/conductor/entities/Camion.java
@@ -3,6 +3,7 @@ package org.example.panda.conductor.entities;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -13,6 +14,7 @@ import java.io.Serializable;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Camion implements Serializable {
 
     @Id
--- src/main/java/org/example/panda/conductor/entities/Conductor.java
+++ src/main/java/org/example/panda/conductor/entities/Conductor.java
@@ -1,4 +1,72 @@
 package org.example.panda.conductor.entities;
 
-public class Conductor {
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.example.panda.trabajador.entities.Trabajador;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "conductores_camion")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Conductor implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @JoinColumn(name = "trabajador_id", nullable = false)
+    private Trabajador trabajador;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @JoinColumn(name = "tipo_licencia_id", nullable = false)
+    private TipoLicencia tipoLicencia;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @JoinColumn(name = "camion_id", nullable = false)
+    private Camion camion;
+
+    @Column(name = "cert_conducir_camion", nullable = false)
+    private boolean certConducirCamion;
+
+    @Column(name = "cert_psicofisico", nullable = false)
+    private boolean certPsicofisico;
+
+    @Column(name = "cert_mecanica_basica", nullable = false)
+    private boolean certMecanicaBasica;
+
+    @Column(name = "cert_primeros_auxilios", nullable = false)
+    private boolean certPrimerosAuxilios;
+
+    @Column(name = "cert_seguridad_vial", nullable = false)
+    private boolean certSeguridadVial;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
