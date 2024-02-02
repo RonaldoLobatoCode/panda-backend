bc8abb3e56463fef4233ec7232485d4804d66954
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-02-02 13:05:22 -0500

    Finalizado el desarrollo de la clase CitasViaje en el paquete 'entity/conductor'

--- src/main/java/org/example/panda/conductor/entities/CitasViaje.java
+++ src/main/java/org/example/panda/conductor/entities/CitasViaje.java
@@ -1,4 +1,39 @@
 package org.example.panda.conductor.entities;
 
-public class CitasViaje {
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name = "citas_viaje")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CitasViaje implements Serializable{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "fecha_cita", nullable = false)
+    private Date fechaCita;
+
+    @Column(name = "origen", nullable = false, length = 100)
+    private String origen;
+
+    @Column(name = "destino", nullable = false, length = 100)
+    private String destino;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @JoinColumn(name = "conductor_id", nullable = false)
+    private Conductor conductor;
 }
