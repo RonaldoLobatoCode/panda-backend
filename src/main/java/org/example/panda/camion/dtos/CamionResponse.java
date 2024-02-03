80f7f2606b93957f2550fc2e7f87fbc849c7d84b
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-02-03 12:06:05 -0500

    Añadiendo CamionResponse para paginación de resultados de camion en el paquete 'dtos/camion'

--- src/main/java/org/example/panda/camion/dtos/CamionResponse.java
+++ src/main/java/org/example/panda/camion/dtos/CamionResponse.java
@@ -1,4 +1,21 @@
 package org.example.panda.camion.dtos;
 
-public class CamionResponse {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class CamionResponse implements Serializable {
+
+    private List<CamionDto> contenido;
+    private int numeroPagina;
+    private int medidaPagina;
+    private long totalElementos;
+    private int totalPaginas;
+    private boolean ultima;
 }
