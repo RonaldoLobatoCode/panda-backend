b26fe969116016aeaad1ff41d959886a413f4dbe
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-02-03 19:20:06 -0500

    Implementacion DTO para la clase Citas Viaje con validaciones de campos en el paquete 'dtos/citas'

--- src/main/java/org/example/panda/citas/dtos/CitasViajeDto.java
+++ src/main/java/org/example/panda/citas/dtos/CitasViajeDto.java
@@ -1,4 +1,33 @@
 package org.example.panda.citas.dtos;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.example.panda.conductor.entities.Conductor;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class CitasViajeDto {
+
+    private Integer id;
+
+    @Past(message = "La reservación de fecha de cita debe ser en el pasado.")
+    @NotNull(message = "La reservación de fecha de cita no puede estar vacía")
+    private Date fechaCita;
+
+    @NotNull(message = "El origen no puede estar vacía.")
+    private String origen;
+
+    @NotNull(message = "El destino no puede estar vacía.")
+    private String destino;
+
+    @NotNull(message = "El conductor no puede estar vacía.")
+    private Conductor conductor;
+
 }
