4b41cc89338db195d2d20849b24cefe749bf65a6
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-02-03 19:26:54 -0500

    Añadiendo interfaz ICitasViajeService para operaciones CRUD de CitasViaje

--- src/main/java/org/example/panda/citas/services/ICitasViajeService.java
+++ src/main/java/org/example/panda/citas/services/ICitasViajeService.java
@@ -1,4 +1,16 @@
 package org.example.panda.citas.services;
 
+import org.example.panda.carreta.dtos.CarretaDto;
+import org.example.panda.citas.dtos.CitasViajeDto;
+import org.example.panda.citas.dtos.CitasViajeResponse;
+import org.example.panda.trabajador.dtos.TrabajadorDto;
+import org.example.panda.trabajador.dtos.TrabajadorResponse;
+
 public interface ICitasViajeService {
+
+    CitasViajeDto createCitasViaje(CitasViajeDto citasViajeDto);
+    CitasViajeResponse listCitasViaje(int numeroDePagina, int medidaDePagina, String ordenarPor, String sortDir);
+    CitasViajeDto listCitasViajeById(Integer id);
+    CitasViajeDto updateCitasViaje(Integer id, CitasViajeDto citasViajeDto);
+    void deleteCitasViaje(Integer Id);
 }
