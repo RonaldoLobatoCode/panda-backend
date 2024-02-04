26ab23398714c211f880b0f88dee1c1948f208d2
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-02-03 19:22:55 -0500

    Añadiendo CitasViajeResponse para paginación de resultados de citas de viaje en el paquete 'dtos/citas'

--- src/main/java/org/example/panda/citas/dtos/CitasViajeResponse.java
+++ src/main/java/org/example/panda/citas/dtos/CitasViajeResponse.java
@@ -1,4 +1,20 @@
 package org.example.panda.citas.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
 public class CitasViajeResponse {
+
+    private List<CitasViajeDto> contenido;
+    private int numeroPagina;
+    private int medidaPagina;
+    private long totalElementos;
+    private int totalPaginas;
+    private boolean ultima;
 }
