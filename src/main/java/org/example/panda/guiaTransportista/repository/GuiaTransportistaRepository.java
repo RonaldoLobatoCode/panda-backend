d432682eeca13f964b8506b06cd7540de5b8fd17
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-02-17 19:39:51 -0600

    Finalizado el desarrollo de la interfaz de la entidad GuiTransportista

---
+++ src/main/java/org/example/panda/guiaTransportista/repository/GuiaTransportistaRepository.java
@@ -0,0 +1,8 @@
+package org.example.panda.guiaTransportista.repository;
+
+import org.example.panda.guiaTransportista.entity.GuiaTransportista;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GuiaTransportistaRepository extends JpaRepository<GuiaTransportista, Integer> {
+
+}
